8fdf6e5a4bbdaf63d89f2b0f75af99d49bbd3649
Matthew Dahl <mattdahl3@gmail.com>
2021-12-08 12:54:09 -0500

    Initial Commit

---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Matt Dahl on 12/8/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
